97f1b4e731cd583e859ded860b90cb81441aa617
Mykola Solianko <mykola_solianko@epam.com>
2025-03-11 14:08:14 +0200

    [aos-provfirewall] Close external port access from gateway except for SSH
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>

--- recipes-aos/aos-provfirewall/files/provfirewall.sh
+++ recipes-aos/aos-provfirewall/files/provfirewall.sh
@@ -7,23 +7,6 @@ if [ -z "$GATEWAY" ]; then
     exit 1
 fi
 
-apply_rules() {
-    INTERFACE=$1
-    SUBNET=$(ip -o -f inet addr show $INTERFACE | awk '{print $4}' | cut -d'/' -f1-2)
-    
-    if [ -z "$SUBNET" ]; then
-        echo "No IP address found for $INTERFACE"
-        return
-    fi
-
-    for PROTO in tcp udp; do
-        iptables -A INPUT -s $GATEWAY/32 -d $SUBNET -i $INTERFACE -p $PROTO -m $PROTO --dport 8089 -j DROP
-        iptables -A INPUT ! -s $SUBNET -d $SUBNET -i $INTERFACE -p $PROTO -m $PROTO --dport 8089 -j DROP
-        iptables -A INPUT -s $SUBNET -d $SUBNET -i $INTERFACE -p $PROTO -m $PROTO --dport 8089 -j ACCEPT
-    done
-}
-
-find /sys/class/net/*/device | cut -d/ -f5 |
-    while read -r INTERFACE; do
-        apply_rules "$INTERFACE"
-    done
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -s ${GATEWAY}/32 -p tcp -m tcp --dport 22 -j ACCEPT
+iptables -A INPUT -s ${GATEWAY}/32 -j DROP
