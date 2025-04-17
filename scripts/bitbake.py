207b0cb1fe1d7877384144b59c185159f9cf9ead
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-04-17 20:52:38 +0300

    [scripts] Update bitbake script
    * add optional field `task`;
    * make `bbake_conf` field optional.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- scripts/bitbake.py
+++ scripts/bitbake.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python3
-""" Calls bitbake command line """
+"""Calls bitbake command line"""
 
 import os
 
 
-def call_bitbake(work_dir, yocto_dir, build_dir, target, bbake_conf, do_clean=False):
+def call_bitbake(
+    work_dir, yocto_dir, build_dir, target, bbake_conf=None, task=None, do_clean=False
+):
     """Calls bitbake."""
-    print(f"Bitbake target: {target}...\n")
 
-    # Create config file
+    if bbake_conf:
+        conf_file = os.path.abspath(os.path.join(work_dir, f"{target}.conf"))
 
-    conf_file = os.path.abspath(os.path.join(work_dir, f"{target}.conf"))
-
-    create_bbake_conf(conf_file, bbake_conf)
+        create_bbake_conf(conf_file, bbake_conf)
 
     cmd = [f"cd {yocto_dir}", f". ./poky/oe-init-build-env {build_dir}"]
 
     if do_clean:
+        print(f"Cleanup {target} build")
+
         cmd.append(f"bitbake {target} -c cleanall")
 
-    cmd.append(f"bitbake {target} --postread {conf_file}")
+    print(f"Bitbake {target}, task {task}")
+
+    cmd.append(
+        f"bitbake {target}"
+        + (f" --postread {conf_file}" if bbake_conf else "")
+        + (f" -c {task}" if task else "")
+    )
 
     line = " && ".join(c for c in cmd)
 
@@ -30,6 +38,8 @@ def call_bitbake(work_dir, yocto_dir, build_dir, target, bbake_conf, do_clean=Fa
 
 def create_bbake_conf(conf_file, bbake_conf):
     """Creates yocto conf file."""
+
+    print(f"Create {conf_file} file")
 
     with open(conf_file, "w+", encoding="utf8") as file:
         for var in bbake_conf:
