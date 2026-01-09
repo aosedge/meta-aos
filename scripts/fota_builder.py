d0bd06866e9eea3c6b78f746cbf0d1666e951354
Mykola Solianko <mykola_solianko@epam.com>
2026-01-09 13:47:00 +0200

    scripts: refactor fota_builder to use unified metadata module
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>

--- scripts/fota_builder.py
+++ scripts/fota_builder.py
@@ -1,100 +1,68 @@
 #!/usr/bin/env python3
-"""Create fota archive"""
+"""Script for building AOS components with unified metadata generation."""
+
 import argparse
-import json
 import os
 import shutil
 import subprocess
 import sys
-from pathlib import Path
 
 import yaml
 from bitbake import call_bitbake
+from metadata import (
+    COMPONENT_FULL_MEDIA_TYPE,
+    COMPONENT_INC_MEDIA_TYPE,
+    AosArchInfo,
+    AosDependency,
+    AosDependencyIdentity,
+    AosIdentity,
+    AosImage,
+    AosOsInfo,
+    AosUpdateItem,
+    BundleBuilder,
+)
 from moulin import rouge
 
 
 class FotaError(Exception):
-    """Exception raised for errors during creating FOTA archive
-
-    Attributes:
-        error code -- Error code
-        message -- explanation of the error
-    """
+    """Exception raised for errors during creating FOTA archive."""
 
     def __init__(self, error_code, message="FOTA error"):
         self.code = error_code
         self.message = f"{message} ({self.code})"
-
         super().__init__(self.message)
 
 
-class FotaBuilder:
-    """Create fota archive class"""
+class ComponentBuilder:
+    """Builder for individual AOS components."""
 
-    def __init__(self, conf, verbose) -> None:
+    def __init__(self, conf: rouge.YamlValue, bundle_dir: str) -> None:
+        self._conf = conf
         self._work_dir = conf["work_dir"].as_str
-        self._components = conf["components"]
-        self._verbose = verbose
-        self._bundle_name = conf["target_images"][0].as_str
-        self._metadata = {"formatVersion": 2, "components": []}
-        self._bundle_dir = os.path.join(self._work_dir, "bundle")
+        self._bundle_dir = bundle_dir
 
-    def create_bundle(self):
-        """Create bundle from prepared files"""
+    def build_component(self, name: str, comp_conf: rouge.YamlValue) -> str:
+        """Build component and return path to the built file in bundle_dir."""
+        method = comp_conf["method"].as_str
+        comp_type = comp_conf.get("componentType", name).as_str
+        version = comp_conf["version"].as_str
 
-        self._mkdir(os.path.dirname(self._bundle_name))
+        filename = comp_conf.get("fileName", f"{comp_type}-{version}.img").as_str
 
-        args = ["tar", "-cf", self._bundle_name, "-C", self._bundle_dir, "."]
-
-        self._run_cmd(args)
-
-    def process_component(self, component, conf):
-        """Process component "component" """
-        metadata = self._create_component_metadata(component, conf)
-        work_dir = os.path.join(self._work_dir, "components", metadata["type"])
-        method = conf["method"].as_str
+        work_dir = os.path.join(self._work_dir, "components", comp_type)
 
         if method == "raw":
-            self._do_raw_component(work_dir, metadata, conf)
+            self._do_raw_component(work_dir, filename, comp_conf)
         elif method == "overlay":
-            self._do_overlay_component(work_dir, metadata, conf)
+            self._do_overlay_component(
+                work_dir, filename, comp_type, version, comp_conf
+            )
         elif method == "custom":
-            self._do_custom_component(metadata, conf)
+            self._do_custom_component(filename, comp_conf)
 
-        self._metadata["components"].append(metadata)
+        return filename
 
-    def create_metadata(self):
-        """Create metadata file in bundle directory"""
-        metadata_file_name = os.path.join(self._bundle_dir, "metadata.json")
-
-        with open(metadata_file_name, "w", encoding="utf-8") as metadata_file:
-            json.dump(self._metadata, metadata_file, indent=4)
-
-    def get_components(self):
-        """Get dictionary with components configuration"""
-        return self._components
-
-    def prepare_bundle_dir(self):
-        """Prepare empty temporary dir for bundle"""
-        self._prepare_dir(self._bundle_dir)
-
-    def _run_cmd(self, args):
-        if self._verbose:
-            print("Running:", " ".join(args))
-
-        ret = subprocess.run(args, check=True)
-        if ret.returncode != 0:
-            raise FotaError(ret.returncode)
-
-    def _do_copy(self, src, dst):
-        if self._verbose:
-            print(f"Copy from {src} to {dst}")
-
-        args = ["cp", "-Lf", src, dst]
-
-        self._run_cmd(args)
-
-    def _prepare_dir(self, wdir):
+    def _prepare_dir(self, wdir: str) -> None:
         if os.path.exists(wdir):
             if os.path.isfile(wdir):
                 os.remove(wdir)
@@ -103,61 +71,23 @@ class FotaBuilder:
 
         os.makedirs(wdir, exist_ok=True)
 
-    def _mkdir(self, new_dir):
-        Path(new_dir).mkdir(parents=True, exist_ok=True)
+    def _run_cmd(self, args: list) -> None:
+        print("Running:", " ".join(args))
 
-    def _update_metadata_var(self, conf, varname, metadata):
-        var = conf.get(varname, None)
+        ret = subprocess.run(args, check=True)
+        if ret.returncode != 0:
+            raise FotaError(ret.returncode)
 
-        if var:
-            metadata[varname] = var.as_str
+    def _do_copy(self, src: str, dst: str) -> None:
+        print(f"Copy from {src} to {dst}")
+        args = ["cp", "-Lf", src, dst]
+        self._run_cmd(args)
 
-    def _add_runtime_dependencies(self, conf, metadata):
-        runtime_deps = conf.get("runtimeDependencies", [])
+    def _do_raw_component(
+        self, work_dir: str, filename: str, conf: rouge.YamlValue
+    ) -> None:
+        """Create archive for raw component."""
 
-        if len(runtime_deps) == 0:
-            return
-
-        meta_dependencies = []
-
-        for dependency in runtime_deps:
-            if len(dependency) == 0:
-                continue
-
-            meta_dependency = {
-                "type": dependency["componentType"].as_str,
-                "requiredVersion": dependency.get("requiredVersion", "").as_str,
-                "minVersion": dependency.get("minVersion", "").as_str,
-                "maxVersion": dependency.get("maxVersion", "").as_str,
-            }
-
-            # Add dependency with non-empty values only
-            meta_dependencies.append({k: v for k, v in meta_dependency.items() if v})
-
-        metadata["runtimeDependencies"] = meta_dependencies
-
-    def _create_component_metadata(self, component, conf):
-        metadata = {
-            "type": conf.get("componentType", component).as_str,
-            "version": conf["version"].as_str,
-        }
-        metadata["fileName"] = conf.get(
-            "fileName", f'{metadata["type"]}-{metadata["version"]}.img'
-        ).as_str
-
-        self._update_metadata_var(conf, "requiredVersion", metadata)
-        self._update_metadata_var(conf, "minVersion", metadata)
-        self._update_metadata_var(conf, "maxVersion", metadata)
-        self._update_metadata_var(conf, "description", metadata)
-        self._update_metadata_var(conf, "annotations", metadata)
-        self._update_metadata_var(conf, "downloadTTL", metadata)
-
-        self._add_runtime_dependencies(conf, metadata)
-
-        return metadata
-
-    def _do_raw_component(self, work_dir, metadata, conf):
-        """Create archive for raw component"""
         self._prepare_dir(work_dir)
 
         block_entry = rouge.construct_entry(conf["partition"])
@@ -167,41 +97,43 @@ class FotaBuilder:
             file.truncate(block_entry.size())
             block_entry.write(file, 0)
 
-        gz_image = os.path.join(self._bundle_dir, metadata["fileName"])
-
+        gz_image = os.path.join(self._bundle_dir, filename)
         os.system(f"gzip < {image_file} > {gz_image}")
 
-    def _do_overlay_component(self, work_dir, metadata, conf):
-        component = metadata["type"]
-        overlay_type = conf["type"].as_str
+    def _do_overlay_component(
+        self,
+        work_dir: str,
+        filename: str,
+        comp_type: str,
+        version: str,
+        conf: rouge.YamlValue,
+    ) -> None:
+        """Create overlay component via bitbake."""
 
+        overlay_type = conf["type"].as_str
         self._prepare_dir(work_dir)
 
-        if self._verbose:
-            print(f"Creating {overlay_type} overlay image for {component}")
-
-        annotats = metadata.get("annotations", {})
-        annotats["type"] = overlay_type
-        metadata["annotations"] = annotats
+        print(f"Creating {overlay_type} overlay image for {comp_type}")
 
         repo = conf.get(
             "ostree_repo",
-            os.path.join(self._work_dir, "ostree_repo", component),
+            os.path.join(self._work_dir, "ostree_repo", comp_type),
         ).as_str
+
+        required_version = conf.get("requiredVersion", "").as_str
 
         bbake_conf = [
             ("AOS_ROOTFS_IMAGE_TYPE", overlay_type),
-            ("AOS_ROOTFS_IMAGE_VERSION", metadata["version"]),
-            ("AOS_ROOTFS_REF_VERSION", metadata.get("requiredVersion", "")),
+            ("AOS_ROOTFS_IMAGE_VERSION", version),
+            ("AOS_ROOTFS_REF_VERSION", required_version),
             ("AOS_ROOTFS_OSTREE_REPO", os.path.abspath(repo)),
             (
                 "AOS_ROOTFS_IMAGE_FILE",
-                os.path.join(os.path.abspath(self._bundle_dir), metadata["fileName"]),
+                os.path.join(os.path.abspath(self._bundle_dir), filename),
             ),
         ]
 
         exclude_items = conf.get("exclude", None)
-
         if exclude_items:
             bbake_conf.append(
                 (
@@ -222,63 +154,140 @@ class FotaBuilder:
         if ret != 0:
             raise FotaError(ret)
 
-    def _do_custom_component(self, metadata, conf):
-        src = conf["file"]
-        self._do_copy(src, os.path.join(self._bundle_dir, metadata["fileName"]))
+    def _do_custom_component(self, filename: str, conf: rouge.YamlValue) -> None:
+        """Copy custom component file."""
 
-    def _exclude_items(self, rootfs_dir, conf):
-        exclude = conf.get("exclude", None)
+        src = conf["file"].as_str
+        self._do_copy(src, os.path.join(self._bundle_dir, filename))
 
-        if not exclude:
-            return
 
-        for item in exclude:
-            value = item.as_str
+def create_update_item(
+    name: str,
+    comp_conf: rouge.YamlValue,
+    filename: str,
+    architecture: str,
+    os_name: str,
+    is_incremental: bool,
+) -> AosUpdateItem:
+    """Create AosUpdateItem from component config."""
 
-            if self._verbose:
-                print(f"Exclude item: {value}")
+    comp_type = comp_conf.get("componentType", name).as_str
+    version = comp_conf["version"].as_str
 
-            if value[0] == os.sep:
-                value = value[1:]
-            os.system(f"rm -rf {os.path.join(rootfs_dir, value)}")
+    media_type = (
+        COMPONENT_INC_MEDIA_TYPE if is_incremental else COMPONENT_FULL_MEDIA_TYPE
+    )
+
+    image = AosImage(
+        mediaType=media_type,
+        path=filename,
+        archInfo=AosArchInfo(architecture=architecture),
+        osInfo=AosOsInfo(os=os_name),
+    )
+
+    dependencies = None
+    runtime_deps = comp_conf.get("dependencies", [])
+    if runtime_deps:
+        dependencies = []
+        for dep in runtime_deps:
+            if isinstance(dep, rouge.YamlValue):
+                dep_codename = dep["codename"].as_str
+                dep_type = dep.get("type", "component").as_str
+                dep_versions = dep["versions"].as_str
+            else:
+                dep_codename = dep["codename"]
+                dep_type = dep.get("type", "component")
+                dep_versions = dep["versions"]
+
+            dependencies.append(
+                AosDependency(
+                    identity=AosDependencyIdentity(
+                        codename=dep_codename,
+                        type=dep_type,
+                    ),
+                    versions=dep_versions,
+                )
+            )
+
+    title = None
+    description = None
+    if comp_conf.get("title", None):
+        title = comp_conf["title"].as_str
+    if comp_conf.get("description", None):
+        description = comp_conf["description"].as_str
+
+    return AosUpdateItem(
+        identity=AosIdentity(
+            codename=comp_type,
+            type="component",
+            title=title,
+            description=description,
+        ),
+        version=version,
+        images=[image],
+        dependencies=dependencies if dependencies else None,
+    )
 
 
 def main():
-    """main procedure"""
-    parser = argparse.ArgumentParser(description="FOTA builder")
+    """Main function."""
 
+    parser = argparse.ArgumentParser(description="AOS Component builder")
     parser.add_argument(
         "conf", metavar="conf.yaml", type=str, help="YAML file with configuration"
     )
-    parser.add_argument(
-        "-v", "--verbose", action="store_true", help="Enable verbose output"
-    )
-
     args = parser.parse_args()
-    conf = yaml.compose(open(args.conf, "r", encoding="utf-8"))
 
-    builder = FotaBuilder(rouge.YamlValue(conf), args.verbose)
+    with open(args.conf, "r", encoding="utf-8") as f:
+        conf = rouge.YamlValue(yaml.compose(f))
 
-    try:
-        builder.prepare_bundle_dir()
+    components_conf = conf.get("components", conf)
 
-        components = builder.get_components()
+    bundle_conf = components_conf.get("bundle", None)
+    if not bundle_conf:
+        print("Error: 'bundle' section not found in config")
+        return 1
 
-        for component in components.keys():  # pylint: disable=consider-using-dict-items
-            print(f"Processing {component}...")
+    bundle_builder = BundleBuilder(bundle_conf, "component")
+    bundle_builder.prepare()
 
-            if not components[component].get("enabled", True).as_bool:
-                if args.verbose:
-                    print(f"{component} skipped")
+    comp_builder = ComponentBuilder(components_conf, bundle_builder._bundle_dir)
 
-                continue
+    items = components_conf.get("items", components_conf.get("components", {}))
+    ret = 0
 
-            builder.process_component(component, components[component])
+    for name in items.keys():
+        comp_conf = items[name]
 
-        builder.create_metadata()
-        builder.create_bundle()
-    except FotaError as err:
-        print(err)
+        if not comp_conf.get("enabled", True).as_bool:
+            print(f"Skipping disabled component: {name}")
+            continue
+
+        print(f"Building component: {name}")
+
+        try:
+            filename = comp_builder.build_component(name, comp_conf)
+
+            item = create_update_item(
+                name,
+                comp_conf,
+                filename,
+                bundle_builder._architecture,
+                bundle_builder._os,
+                bundle_builder._is_incremental,
+            )
+            bundle_builder.add_item(item)
+
+        except FotaError as err:
+            print(f"Failed to build component {name}: {err}")
+            ret = 1
+
+    if ret != 0:
+        print("Some components failed to build")
+
+        return ret
+
+    bundle_builder.build()
 
     return 0
 
