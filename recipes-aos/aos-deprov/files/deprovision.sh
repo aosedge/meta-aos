2712aace4d58f0dd5ed29e5592aaa3194ea74ddb
Mykola Solianko <mykola_solianko@epam.com>
2026-05-27 21:53:12 +0300

    provfirewall: switch provisioning firewall to nftables
    Replace the iptables INPUT rules with an nftables table (inet aos-provfw);
    deprovision tears it down with nft. Depend on nftables and the conntrack
    modules instead of iptables.
    
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

--- recipes-aos/aos-deprov/files/deprovision.sh
+++ recipes-aos/aos-deprov/files/deprovision.sh
@@ -14,15 +14,7 @@ clear_disks() {
 }
 
 remove_firewall_rules() {
-    GATEWAY=$(ip route | grep default | awk '{print $3}' | head -n1)
-
-    if [ -z "$GATEWAY" ]; then
-        echo "No default gateway found"
-        return
-    fi
-
-    iptables -D INPUT -s ${GATEWAY}/32 -p tcp -m tcp --dport 22 -j ACCEPT
-    iptables -D INPUT -s ${GATEWAY}/32 -j DROP
+    nft delete table inet aos-provfw 2>/dev/null
 }
 
 deprovision_async() {
